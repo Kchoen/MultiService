08b3e134c5ac7d9291b0de049db69b5f2da7ae4a
KAKAKA <kj0716011kj.cs07@nycu.edu.tw>
2024-09-26 15:47:49 +0800

    correct silly mistake

--- static/web/login_interface.js
+++ static/web/login_interface.js
@@ -4,9 +4,9 @@ function toFirstPage() {
     $(document.body).append(
         $(`
         <a href="https://tetr.kchoen.com" style="position:absolute;top:30%;left:40%" target="_blank" title="Tetr-2P Battle網站">🔥🔥Tetr-2P Battle網站🔥🔥</a>
-        <a href="https://https://kchoen.github.io/StockCal/" style="position:absolute;top:40%;left:40%"target="_blank" title="股票計算機">🔥股票計算機</a>
-        <a href="https://https://kchoen.github.io/ReminderApp/" style="position:absolute;top:50%;left:40%"target="_blank" title="飲食記錄APP">🔥飲食記錄APP</a>
-        <a href="https://https://kchoen.github.io/DinnerList/" style="position:absolute;top:60%;left:40%"target="_blank" title="餐廳名冊記錄App">🔥餐廳名冊記錄App</a>
+        <a href="https://kchoen.github.io/StockCal/" style="position:absolute;top:40%;left:40%"target="_blank" title="股票計算機">🔥股票計算機</a>
+        <a href="https://kchoen.github.io/ReminderApp/" style="position:absolute;top:50%;left:40%"target="_blank" title="飲食記錄APP">🔥飲食記錄APP</a>
+        <a href="https://kchoen.github.io/DinnerList/" style="position:absolute;top:60%;left:40%"target="_blank" title="餐廳名冊記錄App">🔥餐廳名冊記錄App</a>
         `)
     );
 }
