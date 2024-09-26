35478c1e86fdbb484817ce826c74080724340803
KAKAKA <kj0716011kj.cs07@nycu.edu.tw>
2024-09-26 14:39:03 +0800

    Update database.js

--- util/database.js
+++ util/database.js
@@ -1,19 +1,22 @@
 const Sequelize = require("sequelize");
 
-const TetrDB = new Sequelize("tetrisdb", "user", "password", {
+const TetrDB = new Sequelize("tetrisdb", "avnadmin", "AVNS_bu0sJD77n1ntBhhSe6b", {
     dialect: "mysql",
-    host: "localhost",
+    host: "mysql-3fb6ee1-engtsai1450-21fb.g.aivencloud.com",
+    port: '24633',
     logging: false,
     query: { raw: true },
 });
-const VoteDB = new Sequelize("votedb", "user", "password", {
+const VoteDB = new Sequelize("votedb", "avnadmin", "AVNS_bu0sJD77n1ntBhhSe6b", {
     dialect: "mysql",
-    host: "localhost",
+    host: "mysql-3fb6ee1-engtsai1450-21fb.g.aivencloud.com",
+    port: '24633',
     logging: false,
 });
-const QuestDB = new Sequelize("questdb", "user", "password", {
+const QuestDB = new Sequelize("questdb", "avnadmin", "AVNS_bu0sJD77n1ntBhhSe6b", {
     dialect: "mysql",
-    host: "localhost",
+    host: "mysql-3fb6ee1-engtsai1450-21fb.g.aivencloud.com",
+    port: '24633',
     logging: false,
 });
 exports.TetrDB = TetrDB;
